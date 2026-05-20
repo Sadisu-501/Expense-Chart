aa910fd944d76baad2c62fe78a7a2a5b9cdfa221
Saskid-501 <Sadisukelvin@gmail.com>
2026-05-20 14:36:49 +0100

    functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const balance = document.getElementById("balance");
 const monthlyTotal = document.getElementById("monthlyTotal");
+const amount = document.getElementById("amount");
 
 async function getData() {
   try {
@@ -8,6 +9,7 @@ async function getData() {
     // Process the fetched data
     balance.textContent = `$${jsonData[7].balance.toFixed(2)}`;
     monthlyTotal.textContent = `$${jsonData[7].monthlyTotal.toFixed(2)}`;
+    amount.textContent = `$${jsonData[0].amount.toFixed(2)}`;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
